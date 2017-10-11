1a5b1ba6375a5f84fb34113b7ffcf51865680af2
JiahuiRuan <jr58@rice.edu>
2017-10-11 13:13:40 -0500

    add input to manager app

---
+++ manager/src/components/LoginForm.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Card, CardSection, Input, Button } from './common';
+import { emailChange, passwordChange } from '../actions';
+import { connect } from 'react-redux';
+
+class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChange(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChange(text);
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            label="PassWord"
+            placeholder="password"
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
+          />
+        </CardSection>
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const mapStateToProps = ({ auth }) => {
+  return {
+    email: auth.email,
+    password: auth.password
+  }
+}
+
+export default connect(mapStateToProps, { emailChange, passwordChange })(LoginForm)
